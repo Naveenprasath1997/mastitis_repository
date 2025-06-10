9d50a9b77130ae4f536bd79afdb521daca84a1ae
Naveenprasath1997 <108398934+Naveenprasath1997@users.noreply.github.com>
2025-06-10 15:16:26 +0530

    Add files via upload

---
+++ blast_p.sh
@@ -0,0 +1,4 @@
+blastp  -query cdhitCluster  -db /home/Databases/Uniport/Uniref100 -num_threads 40 -outfmt "6 qseqid qlen sseqid stitle slen evalue length pident qcovs" -max_target_seqs 1 -max_hsps 1 -out blastpOut1.txt
+
+
+
