be287bfe6c0454b627f39e8f8c40315f1a09f4d6
Naveenprasath1997 <108398934+Naveenprasath1997@users.noreply.github.com>
2025-06-10 15:45:47 +0530

    Delete blast_p.sh

--- blast_p.sh
+++
@@ -1,4 +0,0 @@
-blastp  -query cdhitCluster  -db /home/Databases/Uniport/Uniref100 -num_threads 40 -outfmt "6 qseqid qlen sseqid stitle slen evalue length pident qcovs" -max_target_seqs 1 -max_hsps 1 -out blastpOut1.txt
-
-
-
